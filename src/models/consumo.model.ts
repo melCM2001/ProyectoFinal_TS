5c6a719ba59ea3d1fa9cc24cfba22390337e7702
MelanieCastro <melaniecastromoraga@hotmail.com>
2022-12-13 07:35:06 -0700

    final

--- src/models/consumo.model.ts
+++ src/models/consumo.model.ts
@@ -1,8 +1,15 @@
 //Modelado de los datos de la tabla de consumo
 import { IPago } from './pago.model';
+import { IsString, IsInt, IsNumberString, IsDateString, IsEmail,  IsBoolean} from 'class-validator';
+
 export interface IConsumo{
     //no es necesario especificar id
+    @IsInt()
     consumo: number;
+
+    @IsInt()
     id_cliente: number;
+
+    @IsBoolean()
     pagado: boolean;
 }
